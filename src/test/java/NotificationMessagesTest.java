26431fcf15448069b62dafec3667c587a5718134
Valentin Molofey <valentin.molofey@gmail.com>
2021-01-31 00:08:37 +0300

    updated NotificationMessagesTest

--- src/test/java/NotificationMessagesTest.java
+++ src/test/java/NotificationMessagesTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.*;
@@ -14,36 +13,42 @@ public class NotificationMessagesTest {
 
     @Test
     public void notificationMessagesChecked() {
-        String message;
-
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
         WebDriver browser = new ChromeDriver();
         browser.manage().window().maximize();
         browser.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         browser.get("http://the-internet.herokuapp.com/notification_message_rendered");
-        WebElement buttonClickHere = browser.findElement(By.cssSelector("div>p>a"));
+        buttonClick(browser);
+        String message = browser.findElement(By.id("flash-messages")).getText();
+        checkMessage(message);
+        buttonClick(browser);
+        checkMessage(message);
+        buttonClick(browser);
+        checkMessage(message);
 
-        buttonClickHere.click();
-        message = browser.findElement(By.id("flash")).getAttribute("innerText").trim().replaceAll("\\n", ""); // составляем все полученные строки в одну
-                //browser.findElement(By.id("flash")).getText();
-        checkMessage(message);
-        buttonClickHere.click();
-        checkMessage(message);
-        buttonClickHere.click();
-        checkMessage(message);
-        buttonClickHere.click();
-        checkMessage(message);
         browser.quit();
     }
 
-    public void checkMessage(String text) {
-        if (text.contains("Action successful")) {
-            text.substring(0,15);
-            assertEquals(text, "Action successful×");
+    public void buttonClick(WebDriver browser) {
+        WebElement buttonClickHere = browser.findElement(By.xpath("//div[@id='content']//a[text()='Click here']"));
+
+        if (buttonClickHere.isDisplayed()) {
+            buttonClickHere.click();
         } else {
-            text.substring(0,34);
-            assertEquals(text, "Action unsuccesful, please try again×"); // опечатка 'unsuccesful' на сайте
+            System.out.println("I don't see the button"); // вывод для проверки себя
+        }
+    }
+
+    public void checkMessage(String text) {
+        if (text.isEmpty()) {
+            System.out.println("Message is empty");
+        } else {
+            if (text.contains("un")) {
+                assertEquals(text, "Action unsuccesful, please try again\n" + "×"); // опечатка 'unsuccesful' на сайте
+            } else {
+                assertEquals(text, "Action successful\n" + "×");
+            }
         }
     }
 }
